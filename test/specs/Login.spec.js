865e54d7e3192f2845785cc2b8f48888c1abbb03
Raffael Azevedo <raffael.azevedo@gmail.com>
2023-09-19 14:29:51 -0300

    apk renomeado para facilitar; arquivo de teste renomeado para padrão começando com letra minuscula

--- test/specs/Login.spec.js
+++
@@ -1,9 +0,0 @@
-const loginScreen = require("../pageobjects/login.screen");
-
-describe('Teste', () => {
-    it('Teste', () => {
-        loginScreen.goToLogin('DEV')
-        loginScreen.ButtonValidar()
-        loginScreen.Login('user', 'senha123')
-    });
-});
